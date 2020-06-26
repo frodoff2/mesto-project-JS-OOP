711e629a8be025b8ffb2db61b174aea184c5adf2
Mukhabbat Murzabayeva <asetmr@gmail.com>
2020-06-26 18:47:59 +0300

    deleted

--- src/scipt/utills/constants.js
+++ src/scipt/utills/constants.js
@@ -9,7 +9,6 @@ export const cards = document.getElementById('locations');
 export const cardsAddButton = document.querySelector('.profile__add-button');   
 export const cardListSelector = '.elements';
 
-export const buttonItems = Array.from(document.querySelectorAll('.popup__button')); 
 export const zoomTitle = document.querySelector('.popup__caption');
 export const zoomImage = document.querySelector('.popup__image');
 
